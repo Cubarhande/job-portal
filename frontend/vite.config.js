f1065fb9ea83a4bc15600b7b726dcf3fb78a6c9f
Cubarhande <54080224+Cubarhande@users.noreply.github.com>
2026-08-27 13:56:45 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ['job-portal-3-ngjx.onrender.com'],
+  },
+  
 })
